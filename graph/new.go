f65632a90335fa4a61a502070802594d03c84447
mishhgun01 <AVATARno15>
2022-11-30 18:00:39 +0300

    new in new

--- graph/new.go
+++ graph/new.go
@@ -18,3 +18,35 @@ package graph
 //	}
 //	return res
 //}
+
+func New(weighted byte, graph map[string]map[string]int) *AbstractGraph {
+	output := make(map[*Node]map[*Node]int, len(graph))
+	vertices := make([]*Node, len(graph))
+	i := 0
+	for vert := range graph {
+		n := &Node{Name: vert, Mark: 0}
+		vertices[i] = n
+		i++
+	}
+	for vert, list := range graph {
+		var parentNode *Node
+		childList := make(map[*Node]int, len(list))
+		for _, v := range vertices {
+			if v.Name == vert {
+				parentNode = v
+				output[v] = childList
+				break
+			}
+		}
+		for vertex, weight := range list {
+			for _, n := range vertices {
+				if n.Name == vertex {
+					childList[n] = weight
+				}
+			}
+		}
+		output[parentNode] = childList
+	}
+	_type := GData{Weighted: weighted}
+	return &AbstractGraph{Graph: output, Properties: &_type}
+}
